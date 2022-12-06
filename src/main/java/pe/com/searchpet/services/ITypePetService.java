9e66cb98b20f695ab2d775d1290e901c1da0f2dc
jerson100 <juamkoo@gmail.com>
2022-12-06 03:07:50 -0500

    Agregamos la interface ITypePetService para desacoplar el code y sea más mantenible

---
+++ src/main/java/pe/com/searchpet/services/ITypePetService.java
@@ -0,0 +1,13 @@
+package pe.com.searchpet.services;
+
+import org.bson.types.ObjectId;
+import pe.com.searchpet.collections.TypePet;
+
+import java.util.List;
+
+public interface ITypePetService {
+    List<TypePet> findAll();
+    TypePet createPet(TypePet t);
+    TypePet findPetById(String id);
+    void deleteTypePetById(String id);
+}
--- src/main/java/pe/com/searchpet/services/TypePetServiceImpl.java
+++ src/main/java/pe/com/searchpet/services/TypePetServiceImpl.java
@@ -4,12 +4,10 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pe.com.searchpet.collections.TypePet;
 import pe.com.searchpet.exceptions.BadRequestException;
 import pe.com.searchpet.exceptions.ResourceNotFoundException;
-import pe.com.searchpet.models.ApiError;
 import pe.com.searchpet.repository.TypePetRepository;
 
 import java.util.Date;
@@ -17,23 +15,26 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TypePetService {
-    private Logger LOG = LoggerFactory.getLogger(TypePetService.class);
+public class TypePetServiceImpl implements ITypePetService{
+    private Logger LOG = LoggerFactory.getLogger(TypePetServiceImpl.class);
     @Autowired
     private TypePetRepository typePetRepository;
 
-    public TypePet findPetById(ObjectId id){
-        Optional<TypePet> typePet = typePetRepository.findById(id);
+    @Override
+    public TypePet findPetById(String id){
+        Optional<TypePet> typePet = typePetRepository.findById(new ObjectId(id));
         if(!typePet.isPresent()){
             throw new ResourceNotFoundException("No se encontró la mascota con el id especificado");
         }
         return typePet.get();
     }
 
+    @Override
     public List<TypePet> findAll(){
         return typePetRepository.findAll();
     }
 
+    @Override
     public TypePet createPet(TypePet typePet) {
         Optional<TypePet> optionalTypePet = typePetRepository.findPetByType(typePet.getType());
         if(!optionalTypePet.isEmpty()) throw new BadRequestException("El tipo de mascota ya existe");
@@ -45,4 +46,15 @@ public class TypePetService {
         return typePetRepository.save(typePet);
     }
 
+    @Override
+    public void deleteTypePetById(String id) {
+        Optional<TypePet> typePetOptional = typePetRepository.findById(new ObjectId(id));
+        if(typePetOptional.isEmpty()){
+            throw new ResourceNotFoundException("El tipo de mascota con el id especificado no existe");
+        }
+        TypePet typePet = typePetOptional.get();
+        typePet.setStatus(0);
+        typePetRepository.save(typePet);
+        LOG.info("Deleted TypePet:{}", typePetOptional.get());
+    }
 }
